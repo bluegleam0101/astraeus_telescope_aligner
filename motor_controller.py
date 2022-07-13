b949e6a1642bf56021ef37103e231b6ab89946e1
bluegleam0101 <sennasoeditos2@gmail.com>
2022-07-13 12:25:07 +0200

    first commit

--- motor_controller.py
+++ motor_controller.py
@@ -2,31 +2,33 @@ import RPi.GPIO as GPIO
 import time
 from RpiMotorLib import RpiMotorLib
 
-#in1 = 17
-#in2 = 18s
-#in3 = 27
-#in4 = 22
+alt_pins = (17, 18, 27, 22)
+#direction= 16 
+#step = 23 
+#EN_pin = 24
 
-direction= 22 # Direction (DIR) GPIO Pin
-step = 23 # Step GPIO Pin
-EN_pin = 24 # enable motor if output = low
+#class altitude_motor():
+ #   def __init__(self)
+altitude_motor = RpiMotorLib.BYJMotor()
+altitude_motor.motor_run(gpiopins=alt_pins, wait=.003, steps=1536, ccwise=False,
+                  verbose=False, steptype="full", initdelay=.001)
 
-# Declare a instance of class pass GPIO pins numbers and the motor type
-mymotortest = RpiMotorLib.A4988Nema(direction, step, (21, 21, 21), "DRV8825")
-GPIO.setup(EN_pin, GPIO.OUT)  # set enable pin as output
-
+class azimuth_motor():
+    def __init__(self, rpimotor_function):
+        self.name = ""
 ###########################
 # Actual motor control
-###########################
-#
-x = 0
 
-GPIO.output(EN_pin, GPIO.LOW)  #s pull enable to low to enable motor
-mymotortest.motor_go(False,  # True=Clockwise, False=Counter-Clockwise
-                        "Full",  # Step type (Full,Half,1/4,1/8,1/16,1/32)
-                        200,  # number of steps
-                        .0005,  # step delay [sec]
-                        False,  # True = print verbose output
-                        .05)  # initial delay [sec]
-time.sleep(5)
+#x = 0
+#for i in range(0,2):
+    #GPIO.output(EN_pin, GPIO.LOW)  #s pull enable to low to enable motor
+    #mymotortest.motor_go(False,  # True=Clockwise, False=Counter-Clockwise
+                            #"Full",  # Step type (Full,Half,1/4,1/8,1/16,1/32)
+                            #200,  # number of steps
+                            #.0005,  # step delay [sec]
+                            #False,  # True = print verbose output
+                            #.05)  # initial delay [sec]
+    #time.sleep(5)
+
+    
 GPIO.cleanup()  # clear GPIO allocations after run
