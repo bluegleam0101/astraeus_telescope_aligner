695f664105d9e59746d1ca453f60768a4073459a
bluegleam0101 <sennasoeditos2@gmail.com>
2022-08-09 22:46:58 +0200

    commit 4

--- motor_controller.py
+++ motor_controller.py
@@ -1,13 +1,13 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
-from RpiMotorLib import RpiMotorLib
+#from RpiMotorLib import RpiMotorLib
 
 alt_pins = (17, 18, 27, 22)
 direction= 16 
 step = 23 
 EN_pin = 24
 
-class altitude_motor():
+class AltitudeMotor():
     def __init__(self, rpimotor_function):
         self.altitude_motor = RpiMotorLib.BYJMotor()
         self.altitude_motor.motor_run(gpiopins=alt_pins, wait=.003, steps=1536, ccwise=False,
@@ -16,7 +16,7 @@ class altitude_motor():
 
 
 
-class azimuth_motor():
+class AzimuthMotor():
     def __init__(self, rpimotor_function, gpiopins, steps_360, inv=False, wait=0.003, gear_ratio=1, steptype="full"):
         '''
         please provide gear ratio as a float corresponding to
@@ -32,34 +32,36 @@ class azimuth_motor():
         self.degrees_to_turn: float
 
     def align_azimuth(self, target_az):
+        #finding delta degrees
         a = target_az - self.current_position
         a = (a + 180) % 360 - 180
         self.degrees_to_turn = a
+        print(degrees_to_turn)
+        #####################
+        #moving motor
 
 
 
-
-        self.rpimotor_function()
-
+        #self.rpimotor_function()
 
 
 
 ###########################s
 # Actual motor control
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(EN_pin, GPIO.OUT)
-x = 0
-for i in range(0,2):
-    GPIO.output(EN_pin, GPIO.LOW)  #s pull enable to low to enable motor
-    mymotortest.motor_go(False,  # True=Clockwise, False=Counter-Clockwise
-                            "Full",  # Step type (Full,Half,1/4,1/8,1/16,1/32)
-                            200,  # number of steps
-                            .0005,  # step delay [sec]
-                            False,  # True = print verbose output
-                            .05)  # initial delay [sec]
-    time.sleep(5)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(EN_pin, GPIO.OUT)
+#x = 0
+#for i in range(0,2):
+#    GPIO.output(EN_pin, GPIO.LOW)  #s pull enable to low to enable motor
+ #   mymotortest.motor_go(False,  # True=Clockwise, False=Counter-Clockwise
+  #                          "Full",  # Step type (Full,Half,1/4,1/8,1/16,1/32)
+   #                         200,  # number of steps
+    #                        .0005,  # step delay [sec]
+     #                       False,  # True = print verbose output
+      #                      .05)  # initial delay [sec]
+    #time.sleep(5)
 
     
-GPIO.cleanup()  # clear GPIO allocations after run
+#GPIO.cleanup()  # clear GPIO allocations after run
 #skskksksksk
